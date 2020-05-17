614721944e34d8f6d86e42fb94f4f16fa8cc2c46
nlemast <nlemaster47@gmail.com>
2020-05-17 12:52:25 -0400

    Create get_youtube_client method

--- create_playlist.py
+++ create_playlist.py
@@ -1,5 +1,12 @@
 import json
+import os
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
 import requests
+import youtube_dl
+
 from secrets import spotify_user_id, spotify_token
 
 class CreatePlaylist:
@@ -7,9 +14,28 @@ class CreatePlaylist:
 	def __init__(self):
 		self.user_id = spotify_user_id
 		self.spotify_token = spotify_token
+		self.youtube_client = get_youtube_client()
 
 	def get_youtube_client(self):
-		pass
+        # Disable OAuthlib's HTTPS verification when running locally.
+        # *DO NOT* leave this option enabled in production.
+		os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+        api_service_name = "youtube"
+        api_version = "v3"
+        client_secrets_file = "client_secret.json"
+
+        # Get credentials and create an API client
+        scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+            client_secrets_file, scopes)
+        credentials = flow.run_console()
+
+        # from the Youtube DATA API
+        youtube_client = googleapiclient.discovery.build(
+            api_service_name, api_version, credentials=credentials)
+
+        return youtube_client
 
 	def get_liked_videos(self):
 		pass
@@ -46,7 +72,7 @@ class CreatePlaylist:
 			}
 		)
 		response_json = response.json()
-		songs = response_json["tracks"]["items"]
+		uri = response_json["tracks"]["items"]
 
 	def add_song_to_playlist(self):
 		pass
