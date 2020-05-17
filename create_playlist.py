65290b1fae045860943d2ef9066802f891138852
nlemast <nlemaster47@gmail.com>
2020-05-17 12:17:19 -0400

    Build out CreatePlaylist class scaffolding

---
+++ create_playlist.py
@@ -0,0 +1,19 @@
+class CreatePlaylist:
+
+	def __init__(self):
+		pass
+
+	def get_youtube_client(self):
+		pass
+
+	def get_liked_videos(self):
+		pass
+
+	def create_playlist(self):
+		pass
+
+	def get_spotify_uri(self):
+		pass
+
+	def add_song_to_playlist(self):
+		pass
